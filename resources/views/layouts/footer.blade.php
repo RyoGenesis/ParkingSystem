ceb279da43624ed66f14502a603ea1c5ebeb13bb
RyoGenesis <rtunggala18@gmail.com>
2021-12-25 20:23:47 +0700

    add font awesome

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer bg-white text-center container-fluid">
-    <div class="p-1 mb-2">
+    <div class="p-2">
        <p class="text-black-50 mb-0">Made By Rio</p>
-       <p class="text-black-50">Copyright 2021. All Rights Reserved.</p>
+       <p class="text-black-50 mb-0">Copyright 2021 &copy; | All Rights Reserved.</p>
     </div>  
 </footer>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,7 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('/asset/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href=" {{mix ('css/style.css')}}">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="{{ asset('/asset/js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
     <title>@yield('title-header')</title>
 </head>
 <body class="d-flex flex-column min-vh-100" onload=display_ct();>
