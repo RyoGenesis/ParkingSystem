19793b453a01abe184c843a243426327e662e449
RyoGenesis <rtunggala18@gmail.com>
2021-12-26 19:52:09 +0700

    refactor script placement

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,8 +8,7 @@
     <link rel="stylesheet" href="{{ asset('/asset/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href=" {{mix ('/css/style.css')}}">
     <link rel="stylesheet" href=" {{mix ('/css/app.css')}}">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
     <title>@yield('title-header')</title>
 </head>
 <body class="d-flex flex-column min-vh-100" onload=display_ct();>
@@ -18,7 +17,10 @@
         @yield('content')
     </div>
     @include('layouts.footer')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="{{ mix('js/app.js') }}"></script>
     <script src="{{ asset('/asset/js/app.js') }}"></script>
+    @yield('script')
 </body>
 </html>
--- resources/views/park_vehicle.blade.php
+++ resources/views/park_vehicle.blade.php
@@ -24,9 +24,6 @@
                 <button class="btn btn-warning fs-5" type="submit">Confirm</button>                
             </form>
         </div>
-        @if (session('success'))
-            <p class="text-success">{{session('success')}}</p>
-        @endif 
     </div>
 </div>
 
@@ -46,7 +43,9 @@
     </div>
   </div>
 </div>
+@endsection
 
+@section('script')
 @if(session('success'))
 <script>
     $(document).ready(function(){
